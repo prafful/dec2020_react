0fd19c20d8cf875d289fb93d217a05ef7471d395
prafful <prraful@gmail.com>
2020-12-11 10:47:38 +0530

    react day 01

---
+++ react_examples/reactapp/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM  from 'react-dom'
+import RootComponent from './rootcomponent/root'
+
+
+ReactDOM.render(
+             <div>
+                <RootComponent></RootComponent>
+                <RootComponent></RootComponent> 
+                <RootComponent></RootComponent>
+                <RootComponent></RootComponent>
+            </div>,
+            
+            document.getElementById('root'))
---
+++ react_examples/reactapp/src/rootcomponent/root.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+
+class RootComponent extends React.Component {
+   
+    render() { 
+        return (
+            <div>
+                <div>
+                    <h1>Root Component!!!! Check</h1>
+                    <h2>Works!</h2>
+                </div>
+                <div>
+                    <hr></hr>
+                </div>
+            </div>
+
+          );
+    }
+}
+ 
+
+export default RootComponent
