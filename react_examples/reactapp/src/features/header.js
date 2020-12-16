deffae9208469aded125108cc8f508910889021a
prafful <prraful@gmail.com>
2020-12-16 10:29:38 +0530

    map

--- react_examples/reactapp/src/features/header.js
+++ react_examples/reactapp/src/features/header.js
@@ -5,7 +5,9 @@ class MyHeader extends React.Component {
     constructor(props){
         super(props)
         this.state = {
-            lifecycle: 88
+            lifecycle: 88,
+            friends:[],
+            friendDetail:[]
         }
     }
 
@@ -26,6 +28,24 @@ class MyHeader extends React.Component {
 
     componentWillMount(){
         console.log("will mount")
+        this.setState({
+            selectedFriend: "",
+            friends: ['obb', 'bnp', 'cas', 'owioh', 'obb', 'cas'],
+            friendDetail:[
+                {
+                    id:1,
+                    location:"chennai"
+                },
+                {
+                    id:2, 
+                    location:"bengaluru"
+                },
+                {
+                    id:3,
+                    location:"rajasthan"
+                }
+            ]
+        })
         //alert()
     }
 
@@ -46,6 +66,45 @@ class MyHeader extends React.Component {
             lifecycle: this.state.lifecycle + 1
         })
     }
+
+    getFriend=(e)=>{
+        console.log("Clicked on friend with index: " )
+        console.log(e.target)
+        this.setState({
+            selectedFriend: e.target.innerText
+        })
+    }
+
+   
+    displayFriends=()=>{
+       return this.state.friends.map((friend, i)=>{
+            return (
+                <li key={i} 
+                    onClick={this.getFriend}>
+                    {friend} @ {i}
+                </li>
+            )
+        })
+    }
+
+    getFriendDetail=(e)=>{
+        console.log(e)
+        console.log(e.target.innerText)
+        this.setState({
+            selectedFriend: e.target.innerText
+        })
+    }
+
+    displayFriendDetail=()=>{
+        return this.state.friendDetail.map((friend)=>{
+             return (
+                 <li key={friend.id} 
+                     onClick={this.getFriendDetail}>
+                     {friend.location} 
+                 </li>
+             )
+         })
+     }
   
     render() { 
         return (  
@@ -58,6 +117,16 @@ class MyHeader extends React.Component {
                 <p>Score: {this.score} </p>
                 <p style={{border:"1px solid red"}}>Teleport: {this.teleportNow()}</p>
                 <p style={this.alien}>Alien call: {this.whoAmI()}</p>
+                <ol>
+                    {this.displayFriends()}
+                </ol>
+                <ol>
+                    {this.displayFriendDetail()}
+                </ol>
+                <p>
+                    <b>Selected friend: </b>
+                    <span>{this.state.selectedFriend} </span>
+                </p>
                 <hr></hr>
             </div>
         );
