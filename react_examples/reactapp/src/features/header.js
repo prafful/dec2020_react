99abaee0fdc28f5ffe614f413ebe96fe2c73b419
prafful <prraful@gmail.com>
2020-12-16 09:38:10 +0530

    component lifecycle

--- react_examples/reactapp/src/features/header.js
+++ react_examples/reactapp/src/features/header.js
@@ -2,6 +2,13 @@ import React from 'react';
 
 class MyHeader extends React.Component {
 
+    constructor(props){
+        super(props)
+        this.state = {
+            lifecycle: 88
+        }
+    }
+
     score = 8
 
     alien = {
@@ -16,11 +23,38 @@ class MyHeader extends React.Component {
     whoAmI=()=>{
         console.log("Am I alien?")
     }
+
+    componentWillMount(){
+        console.log("will mount")
+        //alert()
+    }
+
+    componentDidMount(){
+        console.log("did mount")
+    }
+
+    componentWillUpdate(){
+        console.log("will update")
+    }
+
+    componentDidUpdate(){
+        console.log("did update")
+    }
+
+    changeLifeCycle=()=>{
+        this.setState({
+            lifecycle: this.state.lifecycle + 1
+        })
+    }
   
     render() { 
         return (  
             <div>
-                <h2 style={{color:"red"}}>Header</h2>
+                <h2 style={{color:"red"}}>
+                    Header @ {this.state.lifecycle}
+                    &nbsp; 
+                    <button onClick={this.changeLifeCycle}>Change</button>
+                </h2>
                 <p>Score: {this.score} </p>
                 <p style={{border:"1px solid red"}}>Teleport: {this.teleportNow()}</p>
                 <p style={this.alien}>Alien call: {this.whoAmI()}</p>
